8f06f10806819e1398ffab958593404bea7db39b
Sopan Patil <patilsopan4148@gmail.com>
2025-08-20 16:45:29 +0000

    the html code i give to you make that code reult big plese and it shhowi

--- src/components/login-dialog.tsx
+++ src/components/login-dialog.tsx
@@ -9,31 +9,23 @@ export function LoginDialog({ open, onOpenChange }: { open: boolean, onOpenChang
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="sm:max-w-5xl">
-        <DialogHeader>
-          <DialogTitle className="text-center text-2xl font-bold text-foreground">
-            Register for Webinar
-          </DialogTitle>
-          <DialogDescription className="text-center">
-            Please fill out the form below to register.
-          </DialogDescription>
-        </DialogHeader>
-        
-        <div 
-          className="visme_d"
-          data-title="Webinar Registration Form"
-          data-url="g7ddqxx0-untitled-project?fullPage=true"
-          data-domain="forms"
-          data-full-page="true" 
-          data-min-height="500px"
-          data-form-id="133190">
+       <DialogContent className="sm:max-w-none w-auto h-auto bg-transparent border-none shadow-none">
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh', width: '80vw' }}>
+            <div 
+              className="visme_d"
+              style={{ width: '100%', height: '100%' }}
+              data-title="Webinar Registration Form"
+              data-url="g7ddqxx0-untitled-project?fullPage=true"
+              data-domain="forms"
+              data-full-page="true" 
+              data-min-height="500px"
+              data-form-id="133190">
+            </div>
+            <Script 
+              src="https://static-bundles.visme.co/forms/vismeforms-embed.js" 
+              strategy="lazyOnload"
+            />
         </div>
-        
-        <Script 
-          src="https://static-bundles.visme.co/forms/vismeforms-embed.js" 
-          strategy="lazyOnload"
-        />
-        
       </DialogContent>
     </Dialog>
   );
