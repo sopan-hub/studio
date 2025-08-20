749db74047fa0936a51d28a1f4e25dbc765508c9
Sopan Patil <patilsopan4148@gmail.com>
2025-08-20 16:53:27 +0000

    take this code and create that login ndsign in card aging and dont just

--- src/components/login-dialog.tsx
+++ src/components/login-dialog.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
 import Script from 'next/script';
 
 
@@ -9,8 +9,8 @@ export function LoginDialog({ open, onOpenChange }: { open: boolean, onOpenChang
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-       <DialogContent className="sm:max-w-none w-auto h-auto bg-transparent border-none shadow-none">
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh', width: '90vw' }}>
+       <DialogContent className="sm:max-w-none w-auto h-auto bg-transparent border-none shadow-none p-0">
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '90vw', height: '90vh' }}>
             <div 
               className="visme_d"
               style={{ width: '100%', height: '100%' }}
@@ -18,7 +18,7 @@ export function LoginDialog({ open, onOpenChange }: { open: boolean, onOpenChang
               data-url="g7ddqxx0-untitled-project?fullPage=true"
               data-domain="forms"
               data-full-page="true" 
-              data-min-height="500px"
+              data-min-height="500px" 
               data-form-id="133190">
             </div>
             <Script 
