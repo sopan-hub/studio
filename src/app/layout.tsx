052704f0f5999af1b46533be129cf1962d0cc03e
Sopan Patil <patilsopan4148@gmail.com>
2025-08-21 04:28:37 +0000

    now do this To get your site ready to show ads, copy and paste this code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+
 import type {Metadata} from 'next';
 import {AuthProvider} from '@/hooks/use-auth';
 import {Toaster} from '@/components/ui/toaster';
 import './globals.css';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Study Buddy AI',
@@ -15,6 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+       <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6772455729424378"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>
         <AuthProvider>
           {children}
